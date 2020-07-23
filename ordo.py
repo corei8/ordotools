618fb264581f63367c96ec25ff673e313fda1e17
corei8 <38426287+corei8@users.noreply.github.com>
2020-07-23 10:15:50 -0400

    cleaning things up

--- ordo.py
+++ ordo.py
@@ -1,24 +1,39 @@
 # TODO: make the easter function easier to read
 # TODO: use datime to diplay Easter in an easier format
 
+
 def easter(year):
     firstDigit = year // 100
     Remain19 = year % 19
     temp = (firstDigit - 15) // 2 + 202 - 11 * Remain19
-    
-    if firstDigit > 26: temp = temp - 1
-    if firstDigit > 38: temp = temp - 1
-    if firstDigit == 21 or firstDigit == 24 or firstDigit == 25 or firstDigit == 33 or firstDigit == 36 or firstDigit == 37: temp = temp - 1
+
+    if firstDigit > 26:
+        temp = temp - 1
+    if firstDigit > 38:
+        temp = temp - 1
+    if (
+        firstDigit == 21
+        or firstDigit == 24
+        or firstDigit == 25
+        or firstDigit == 33
+        or firstDigit == 36
+        or firstDigit == 37
+    ):
+        temp = temp - 1
 
     temp = temp % 30
     tA = temp + 21
-    if temp == 29: tA = tA - 1
-    if temp == 28 and Remain19 > 10: tA = tA -1
+    if temp == 29:
+        tA = tA - 1
+    if temp == 28 and Remain19 > 10:
+        tA = tA - 1
     # find the next Sunday
     tB = (tA - 19) % 7
     tC = (40 - firstDigit) % 4
-    if tC == 3: tC = tC + 1
-    if tC > 1: tC = tC + 1
+    if tC == 3:
+        tC = tC + 1
+    if tC > 1:
+        tC = tC + 1
     temp = year % 100
     tD = (temp + (temp // 4)) % 7
     tE = ((20 - tB - tC - tD) % 7) + 1
@@ -31,6 +46,13 @@ def easter(year):
         m = 3
     print(year, m, d)
 
-user = int(input("Enter a year: "))
-easter(user)
+
+# simple terminal application for testing purposes:
+user = 1
+while user != 0:
+    user = int(input("Enter a year: "))
+    if user == 0:
+        break
+    else:
+        easter(user)
 
