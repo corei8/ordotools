b54cf22b78bc843ebd2f14910d6a675b20fc6a3f
corei8 <38426287+corei8@users.noreply.github.com>
2021-07-25 12:22:39 -0400

    add html file generator

--- romancalendar.py
+++ romancalendar.py
@@ -90,16 +90,11 @@ def findsunday(date):
     if date.strftime("%a") == "Sat":
         x = 6
     if date.strftime("%a") == "Sun":
-        pass
+        x = 0
     return timedelta(days=x)
 
 
 # TODO: schedule all the octaves
-
-
-# TODO: add the ember days
-
-
 def temporal(year):
     year_prev, year = int(year) - 1, int(year)
     this_year = year
@@ -468,10 +463,11 @@ def temporal(year):
                 ]
             )
             if (easter(year) + week(i)).strftime("%B") == "September":
-                sept_counter += 1
-            #! If the first sunday of september is the first, then sept_counter == 3 is true
-            #! the wednesday cannot fall on or before the 14th of september.
-            #! this will be correct for many but not all cases!!:
+                if int((easter(year) + week(i)).strftime("%d")) <= 3:
+                    sept_counter += 0
+                else:
+                    sept_counter += 1
+            #! Make sure that this is correct for all cases
             if sept_counter == 2:
                 cycle.append(
                     [
@@ -571,6 +567,74 @@ def temporal(year):
         for row in cycle:
             print(row[0] + ", " + row[1] + ", " + str(row[2]) + ", " + str(row[3]))
         sys.stdout = original_stdout
+    original_stdout = sys.stdout
+    # print to HTML table
+    with open("romancal.html", "w") as f:
+        sys.stdout = f
+        print(
+            """
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>"""
+            + str(year)
+            + """ Ordo</title>
+            <style>
+                .styled-table {
+                    border-collapse: collapse;
+                    margin: 25px 0;
+                    font-size: 0.9em;
+                    font-family: sans-serif;
+                    min-width: 400px;
+                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                }
+                .styled-table thead tr {
+                    background-color: #009879;
+                    color: #ffffff;
+                    text-align: left;
+                }
+                .styled-table tbody tr {
+                    border-bottom: 1px solid #dddddd;
+                }
+
+                .styled-table tbody tr:nth-of-type(even) {
+                    background-color: #f3f3f3;
+                }
+
+                .styled-table tbody tr:last-of-type {
+                    border-bottom: 2px solid #009879;
+                }
+                .styled-table tbody tr.active-row {
+                    font-weight: bold;
+                    color: #009879;
+                }
+            </style>
+        </head>
+        <body>
+        <table class="styled-table">
+        """
+        )
+
+        print("<tr><th>Feast</th><th>Rank</th><th>Weekday</th><th>Date</th></tr>")
+        for row in cycle:
+            print("<tr>")
+            print(
+                "<td>"
+                + row[0]
+                + "</td><td>"
+                + row[1]
+                + "</td><td>"
+                + str(row[2])
+                + "</td><td>"
+                + str(row[3])
+                + "</td>"
+            )
+            print("</tr>")
+        print("</table></body></html>")
+    sys.stdout = original_stdout
 
 
 def app():
